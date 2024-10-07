80eae67b4efc242bba1ca50afc6770679510a385
f h <falhamad2002@gmail.com>
2024-10-07 15:56:52 +0300

    task is done

---
+++ functions2.js
@@ -0,0 +1,28 @@
+// receives a number n
+// returns true if it's odd, false otherwise
+function isOdd(n) {
+  if (n % 2 === 0) console.log(`true`);
+  else console.log(`false`);
+}
+
+isOdd(4);
+
+// receives a number n
+// returns the number of ODD numbers smaller than n
+function oddsSmallerThan(n) {
+  console.log((n - 1) / 2);
+}
+
+oddsSmallerThan(15);
+
+// receives a number n
+// returns its square if it's odd
+// returns its double if it's even
+function squareOrDouble(n) {
+  if (n % 2 !== 0) {
+    console.log(n * n);
+  } else {
+    console.log(n * 2);
+  }
+}
+squareOrDouble(6);
