48b3ceee2230b2128d8584cadb29cad12016d948
f h <falhamad2002@gmail.com>
2024-10-08 19:51:49 +0300

    comments are fixed

--- functions2.js
+++ functions2.js
@@ -1,16 +1,16 @@
 // receives a number n
 // returns true if it's odd, false otherwise
 function isOdd(n) {
-  if (n % 2 === 0) console.log(`true`);
-  else console.log(`false`);
+  if (n % 2 !== 0) return true;
+  else return false;
 }
 
-isOdd(4);
+console.log(isOdd(4));
 
 // receives a number n
 // returns the number of ODD numbers smaller than n
 function oddsSmallerThan(n) {
-  console.log((n - 1) / 2);
+  console.log(Math.floor((n - 1) / 2));
 }
 
 oddsSmallerThan(15);
